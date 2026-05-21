4a7c40d57270ae88de1af20447ba87b5556522eb
yokotabira-tcs <yoko.tabira@tcs.com>
2026-05-21 17:02:43 +0900

    260521 NextJS dashboard Chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+     console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -37,7 +37,7 @@ export async function fetchLatestInvoices() {
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`;
+      LIMIT 2`;
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
